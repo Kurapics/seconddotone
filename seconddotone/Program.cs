66dbb2cea08f51c622ea7eda222f72fd627d700f
Компьютер <@DESKTOP-0RODP2R>
2023-04-25 21:51:28 +0200

    Добавьте файлы проекта.

---
+++ seconddotone/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите цену за кг конфет: ");
+double x = double.Parse(Console.ReadLine());
+Console.Write("Введите граммовку общего кол-ва конфет: ");
+double y = double.Parse(Console.ReadLine());
+Console.Write("Ваш бюджет: ");
+double k = double.Parse(Console.ReadLine());
+
+Console.WriteLine(y + "кг  конфет будут стоить " + (x * y) + "р");
+Console.WriteLine("на " + k + "р вы можете купить" + k/x + "кг конфет");
